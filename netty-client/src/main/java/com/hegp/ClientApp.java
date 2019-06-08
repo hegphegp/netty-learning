76217117a9237fc9ff948f02a62aa309b96daf6f
heguopei <2386824052@qq.com>
2019-06-08 22:09:33 +0800

    初始化仓库

---
+++ netty-client/src/main/java/com/hegp/ClientApp.java
@@ -0,0 +1,65 @@
+package com.hegp;
+
+import com.hegp.coder.MessageDecoder;
+import com.hegp.coder.MessageEncoder;
+import com.hegp.entity.Message;
+import com.hegp.handler.ClientBusinessHandler;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+
+public class ClientApp {
+    public static void main(String[] args) throws Exception {
+
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline()
+                            .addLast(new LengthFieldBasedFrameDecoder(1024 * 1024 * 10, 2, 4, 0, 0, true))
+                            .addLast(new MessageDecoder())
+                            .addLast(new ClientBusinessHandler())
+                            .addLast(new MessageEncoder());  //给服务端发送数据时编码
+                        }
+                    });
+
+            //异步连接到服务
+            ChannelFuture future = b.connect("127.0.0.1", 8089).sync();
+
+            Channel clientChannel = future.channel();
+
+            int currentCount = 10;
+
+            while (currentCount > 0) {
+                Thread.sleep(10);
+                String message = String.format("client %s", System.currentTimeMillis());
+                byte[] body = message.getBytes();
+                Message msgEntity = new Message((byte) 0xAB, (byte) 0xCD, body);
+                //发送数据
+                clientChannel.writeAndFlush(msgEntity);
+                //System.out.println(msgBody);
+                currentCount -= 1;
+            }
+
+            System.out.println("Close Channel");
+            clientChannel.close();
+            clientChannel.closeFuture().sync();
+
+            System.out.println("Client Exit");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+
+    }
+}
---
+++ netty-common/src/main/java/com/hegp/entity/Message.java
@@ -0,0 +1,70 @@
+package com.hegp.entity;
+
+/**
+ * 消息协议数据包
+ */
+public class Message {
+
+    private byte isZip;     // 是否压缩
+    private byte zipType;   // 压缩的类型和算法
+    private int version;    // 版本
+    private int requestId;  // 请求id
+    private byte type;      // 消息类型  0xAF表示心跳包, 0xBF表示超时包, 0xCF业务信息包, 订下的规矩是: 心跳包的内容长度为0
+    // 类型  系统编号 0xAB 表示A系统，0xBC 表示B系统
+    // private byte type;
+
+    // 信息标志  0xAB 表示心跳包    0xBC 表示超时包  0xCD 业务信息包
+    private byte flag;
+
+    // 主题信息的长度
+    private int length;
+
+    // 内容
+    private byte[] body;
+
+    public Message() { }
+
+    public Message(byte type, byte flag, byte[] body) {
+        this.type = type;
+        this.flag = flag;
+        this.length = length;
+        this.body = body;
+    }
+
+    public byte getType() {
+        return type;
+    }
+
+    public void setType(byte type) {
+        this.type = type;
+    }
+
+    public byte getFlag() {
+        return flag;
+    }
+
+    public void setFlag(byte flag) {
+        this.flag = flag;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public byte[] getBody() {
+        return body;
+    }
+
+    public void setBody(byte[] body) {
+        this.body = body;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("type:%s flag:%s len:%s", type, flag, body.length);
+    }
+}
